c223dd0abc288d2451b22839902e0fdf3bd0a18b
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2022-01-05 22:52:23 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream documentFile, stopWorkFile;
+ofstream indexFile;
+
+int main() {
+    documentFile.open("vanban.txt");
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
